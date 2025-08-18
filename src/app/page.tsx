386a00c82be07adbb9022aee07059c9c77a7a5c8
chobyungik <loverkawk@gmail.com>
2025-08-18 14:27:23 +0900

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -496,8 +496,10 @@ export default function Page() {
             i === 0
               ? {
                  title:"경쟁력",
-        subtitle: <span className="text-white">전통방식으로 조리? 특제 육수와 특제 소스? 정성을 다해서 끓이는 노력? 이런 건 어디든지 다 하는 겁니다.
-화려한 미사여구는 빼고 담백하게 창업자가 알아야 할 진짜 여상수의 장점을 알려드립니다.</span>,                
+        subtitle: <span className="text-white">전통방식으로 조리? 특제 육수와 특제 소스? 정성을 다해서 끓이는 노력?
+          이런 건 어디든지 다 하는 겁니다.
+화려한 미사여구는 빼고 담백하게 창업자가 알아야 할
+          진짜 여상수의 장점을 알려드립니다.</span>,                
                 }
               : undefined
           }
